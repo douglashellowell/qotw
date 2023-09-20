6f1bf73befbe659e319a7d39680fdbad37ac577b
Douglas Hellowell <53038423+douglashellowell@users.noreply.github.com>
2023-09-20 15:42:00 -0600

    add voting/

--- frontend/src/components/StickyNote.svelte
+++ frontend/src/components/StickyNote.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
 	import { css } from '@emotion/css';
-
-	import { darken, desaturate, opacify, transparentize } from 'polished';
+	import { darken, desaturate, transparentize } from 'polished';
 	import type { Vote } from '../types';
 	export let content: string;
 	export let submittedBy: string = 'Anonymous';
 
-	export let votes: Vote[] = [];
+	export let votes: Vote[];
+
+	$: console.log('xoxoxox', votes);
 
 	const fonts = ['Caveat', 'Fasthand', 'Zeyada', 'Courgette', 'Kalam'];
 	const randomFont = fonts[Math.floor(Math.random() * fonts.length)];
 
-	const colours = ['#FABD9E', '#F9DCC4', '#F9F4D2', '#D4E09B', '#9BC1BC', '#5CA4A9'];
+	const colours = ['#5FC5BA', '#F4B55A', '#F5E589', '#F05A41'];
 	const randomColour = colours[Math.floor(Math.random() * colours.length)];
-	console.log(randomColour);
 
 	const contentStyles = css`
 		font-family: ${randomFont}, cursive;
@@ -21,7 +21,7 @@
 		color: ${desaturate(0.5, darken(0.5, randomColour))};
 		display: flex;
 		flex-direction: column;
-		width: 500px;
+		width: 400px;
 		box-shadow: -10px 5px 5px 1px ${transparentize(0.8, darken(0.8, randomColour))};
 		font-size: 1.2rem;
 		padding: 10px 20px;
@@ -33,13 +33,17 @@
 <div class={contentStyles}>
 	<p class="content">{content}</p>
 	<p class="submitted-by">{submittedBy}</p>
-	{#each votes as vote}
-		<img
-			src={`icon-${vote.sticker}.png`}
-			alt={vote.sticker}
-			style={`height: ${vote.amount * 3}px; width: ${vote.amount * 3}px;`}
-		/>
-	{/each}
+	{#if votes !== undefined}
+		<div class="vote-list">
+			{#each votes as vote}
+				<img
+					src={`icon-${vote.sticker}.png`}
+					alt={vote.sticker}
+					style={`height: ${vote.amount * 20}px; width: ${vote.amount * 20}px;`}
+				/>
+			{/each}
+		</div>
+	{/if}
 </div>
 
 <style lang="scss">
@@ -47,5 +51,8 @@
 		align-self: flex-end;
 		font-size: 1rem;
 		font-style: italic;
+	}
+	.vote-list {
+		display: flex;
 	}
 </style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,17 +1,66 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import type { Sticker, Response, Question } from '../types';
+	import type { Sticker, Response, Question, Vote } from '../types';
 	import ControlBar from '../components/ControlBar.svelte';
 	import StickyNote from '../components/StickyNote.svelte';
 	import Header from './Header.svelte';
 
 	function chooseVote(sticker: Sticker) {
-		console.log(sticker);
+		console.log('choosing', sticker);
+
+		let newVotes = [...votes];
+
+		if (votes[selectedStickyNoteIndex]) {
+			const stickerExists = newVotes[selectedStickyNoteIndex].find(
+				(vote) => vote.sticker === sticker
+			);
+			if (stickerExists) {
+				stickerExists.amount++;
+			} else {
+				newVotes[selectedStickyNoteIndex] = [
+					...newVotes[selectedStickyNoteIndex],
+					{ sticker, amount: 1 }
+				];
+			}
+		} else {
+			newVotes[selectedStickyNoteIndex] = [{ sticker, amount: 1 }];
+		}
+		votes = newVotes;
 	}
 
-	let selectedStickyNote: StickyNote;
-	let question: Question;
-	let responses: Response[];
+	function selectSticky(index: number) {
+		selectedStickyNoteIndex = index;
+	}
+
+	let selectedStickyNoteIndex: number;
+
+	let question: Question = {
+		Question:
+			'This week we\'ll see summer 2023 coming to an end, so for this week\'s Punch Line complete the following: "With summer ending, one thing I can do to keep that summer feeling going is ___________."',
+		Id: '',
+		StartDate: new Date(),
+		EndDate: new Date(),
+		CreatedDate: new Date()
+	};
+
+	let responses: Response[] = [
+		{
+			Answer: 'sunbatinhg!',
+			Id: '',
+			QuestionId: '',
+			SubmittedBy: 'Doug',
+			CreatedDate: new Date()
+		},
+		{
+			Answer: 'eating!!',
+			Id: '',
+			QuestionId: '',
+			SubmittedBy: 'PJ',
+			CreatedDate: new Date()
+		}
+	];
+	let votes: Vote[][] = [];
+	$: console.log('page', votes);
 
 	onMount(async () => {
 		const q = await fetch('http://localhost:7071/api/question/current', {
@@ -38,14 +87,20 @@
 	<main class="whiteboard">
 		{#if question && responses}
 			<p>{question.Question}</p>
-			{#each responses as response}
-				<StickyNote content={response.Answer} submittedBy={response.SubmittedBy} />
+			{#each responses as response, index}
+				<div on:click={() => selectSticky(index)}>
+					<StickyNote
+						content={response.Answer}
+						submittedBy={response.SubmittedBy}
+						votes={votes[index]}
+					/>
+				</div>
 			{/each}
 		{:else}
 			<p>loading.....</p>
 		{/if}
 	</main>
-	<ControlBar {chooseVote} isOpen={selectedStickyNote !== undefined} />
+	<ControlBar {chooseVote} isOpen={selectedStickyNoteIndex !== undefined} />
 </div>
 
 <style>
