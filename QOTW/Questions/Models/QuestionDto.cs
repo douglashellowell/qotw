a8d5f39c6a8bc8264b67388ee7376e85e609f537
PJ <pdahlberg@nurses.ab.ca>
2023-09-20 13:24:11 -0600

    add question model

---
+++ QOTW/Questions/Models/QuestionDto.cs
@@ -0,0 +1,23 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Questions.Models;
+
+public class QuestionDto
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+
+    [BsonElement("question")] 
+    public string Question { get; set; }
+    
+    [BsonElement("startDate")]
+    public DateTime StartDate { get; set; }
+    
+    [BsonElement("endDate")]
+    public DateTime EndDate { get; set; }
+    
+    [BsonElement("createdDate")]
+    public DateTime? CreatedDate { get; set; }
+}
--- QOTW/Questions/Questions.cs
+++ QOTW/Questions/Questions.cs
@@ -10,81 +10,88 @@ namespace Questions;
 
 public class Questions
 {
-      private readonly MongoClient _mongoClient;
-        private readonly ILogger _logger;
+    private readonly MongoClient _mongoClient;
+    private readonly ILogger _logger;
 
-        private readonly IMongoCollection<Post> _posts;
+    private readonly IMongoCollection<QuestionDto> _posts;
 
-        public Questions(ILoggerFactory loggerFactory, MongoClient mongoClient)
-        {
-            _mongoClient = mongoClient;
-            _logger = loggerFactory.CreateLogger<Questions>();
-
-            var database = _mongoClient.GetDatabase("TestThings");
-            _posts = database.GetCollection<Post>("FirstTry");
-        }
-
-        [Function(nameof(SetComments))]
-        public async Task<HttpResponseData> SetComments(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "set")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-           
-            var post = new Post()
-            {
-                PostAuthor = "Me!",
-                PostTitle = "What sort of question would?",
-            };
-            try
-            {
-                _posts.InsertOne(post);
-            }
-            catch (Exception e)
-            {
-                return req.CreateResponse(HttpStatusCode.InternalServerError);
-            }
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-
-            await response.WriteAsJsonAsync(post);
-            return response;
-        }
-        
-        [Function(nameof(GetComments))]
-        public async Task<HttpResponseData> GetComments(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "get")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            try
-            {
-                var comments = await _posts.FindAsync(c => true);
-                var response = req.CreateResponse(HttpStatusCode.OK);
-
-                await response.WriteAsJsonAsync(comments.ToList());
-                return response;
-            }
-            catch (Exception e)
-            {
-                return req.CreateResponse(HttpStatusCode.InternalServerError);
-            }
-
-           
-        }
-    [Function("Questions")]
-    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
-        FunctionContext executionContext)
+    public Questions(ILoggerFactory loggerFactory, MongoClient mongoClient)
     {
-        var logger = executionContext.GetLogger("Questions");
-        logger.LogInformation("C# HTTP trigger function processed a request.");
+        _mongoClient = mongoClient;
+        _logger = loggerFactory.CreateLogger<Questions>();
+
+        var database = _mongoClient.GetDatabase("qotw");
+        _posts = database.GetCollection<QuestionDto>("questions");
+    }
+
+    [Function(nameof(AddQuestion))]
+    public async Task<HttpResponseData> AddQuestion(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "addquestion")]
+        HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        var content = await req.ReadFromJsonAsync<QuestionDto>();
+
+        if (content is null)
+        {
+            return req.CreateResponse(HttpStatusCode.BadRequest);
+        }
+    
+        try
+        {
+            content.CreatedDate = DateTime.Now;
+            _posts.InsertOne(content);
+        }
+        catch (Exception e)
+        {
+            return req.CreateResponse(HttpStatusCode.InternalServerError);
+        }
 
         var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-        response.WriteString("Welcome to Azure Functions!");
-
+        await response.WriteAsJsonAsync(content);
         return response;
-        
+    }
+
+    [Function(nameof(GetQuestions))]
+    public async Task<HttpResponseData> GetQuestions(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getquestions")]
+        HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        try
+        {
+            var comments = await _posts.FindAsync(c => true);
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            await response.WriteAsJsonAsync(comments.ToList());
+            return response;
+        }
+        catch (Exception e)
+        {
+            return req.CreateResponse(HttpStatusCode.InternalServerError);
+        }
+    }
+    
+    [Function(nameof(GetCurrentQuestion))]
+    public async Task<HttpResponseData> GetCurrentQuestion(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getcurrentquestion")]
+        HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        try
+        {
+            var comments = await _posts.FindAsync(c => c.EndDate > DateTime.Now && c.StartDate < DateTime.Now);
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            await response.WriteAsJsonAsync(comments.ToList());
+            return response;
+        }
+        catch (Exception e)
+        {
+            return req.CreateResponse(HttpStatusCode.InternalServerError);
+        }
     }
 }
